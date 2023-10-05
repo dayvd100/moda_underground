e329c968ed1993a3a1b87600d2efdeb4b7ed03a9
Legit <pedroclash3245@gmail.com>
2023-10-05 01:11:23 -0300

    header, footer e começando a mainpageu

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Header from './components/layouts/header';
-import Contato from './components/pages/contato';
-import Home from './components/pages/home';
-import Loja from './components/pages/loja';
-import Referencia from './components/pages/referencia';
-import Social from './components/pages/social';
-import NotFound from './components/pages/notFound';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/layouts/header";
+import Contato from "./components/pages/contato";
+import Home from "./components/pages/home";
+import Loja from "./components/pages/loja";
+import Referencia from "./components/pages/referencia";
+import Social from "./components/pages/social";
+import NotFound from "./components/pages/notFound";
+import Footer from "./components/layouts/footer";
 // import Footer from './components/layouts/footer';
 
 function App() {
   return (
-    <div className='container'>
-    <Router>
+    <div className="container">
+      <Router>
         <Header /> {/*header fixo */}
-        <div className='links-rotas'>
-        <Routes>
-          <Route path="/" element={<Home/>}/>                  
-          <Route path="/referencias"  element={<Referencia/>}/>
-          <Route path="/social" element={<Social/>}/>             {/*o que vai ser renderizado pela url (path)*/}
-          <Route path="/loja"  element={<Loja/>}/>                
-          <Route path="/contato"  element={<Contato/>}/>
-          <Route path="*"  element={<NotFound/>}/>              
-        </Routes>
+        <div className="links-rotas">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/referencias" element={<Referencia />} />
+            <Route path="/social" element={<Social />} />{" "}
+            {/*o que vai ser renderizado pela url (path)*/}
+            <Route path="/loja" element={<Loja />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
+        <Footer />
         {/* <Footer/> footer fixo */}
-    </Router>
+      </Router>
     </div>
   );
 }
--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,9 +1,55 @@
-function Footer(){
-    return(
-        <footer>
-            <h1>Footer</h1>
-        </footer>
-    )
+import {
+  RiInstagramFill,
+  RiPinterestFill,
+  RiTwitterXFill,
+} from "react-icons/ri";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <div className="footer">
+          <div className="socialicons">
+            <a href="#">
+              <RiInstagramFill />
+            </a>
+            <a href="#">
+              <RiTwitterXFill />
+            </a>
+            <a href="#">
+              <RiPinterestFill />
+            </a>
+          </div>
+          <div className="footernav">
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Referências</a>
+              </li>
+              <li>
+                <a href="">Social</a>
+              </li>
+              <li>
+                <a href="">Loja</a>
+              </li>
+              <li>
+                <a href="">Contato</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footerbottom">
+            <p>
+              Copyright &copy;2023: Designed by{" "}
+              <span className="desing">$TyL</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
