c58f1e1494b6d86f534796caa9754b4174dc43c0
dayvd <allysondayvd@gmail.com>
2023-10-13 16:16:37 -0300

    loja modificada

--- src/components/pages/loja.tsx
+++ src/components/pages/loja.tsx
@@ -1,10 +1,26 @@
-function Loja(){
-        return (
-            <div>
-                <h1>Loja</h1>
-            </div>
-        )
+import React from 'react';
+import "./loja.css"
+import image1 from '../../img/img1.jpg';
 
+function Loja() {
+  const imgList = [image1, image1, image1, image1, image1, image1, image1, image1, image1, image1, image1, image1, image1, image1, image1, image1];
+
+  return (
+    <div className="store-container">
+      {imgList.map((img_clothes, index) => (
+        <div className='description-clothes'>
+        <div className='img-clothe-container' key={index}>
+          <img src={img_clothes} alt={`clothes ${index}`} className='img-clothe' />
+          <hr className='line-clothe-description'/>
+          <p>CLOTHE'S NAME</p>
+          <p>PRICE</p>
+        </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Loja;
+
+
